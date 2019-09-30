00ad56707dd4304ec4120633cd42ec21055b0e35
khorramkho <52155201+khorramkho@users.noreply.github.com>
2019-09-30 17:59:53 +0100

    added vue router and routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,13 +20,32 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+import VueRouter from 'vue';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+Vue.use(VueRouter);
+const routes = [
+    {
+        path: '/',
+        component: Dashboard
+    },
+    {
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/registser',
+        component: Register
+    }
+];
+const router = new VueRouter({
+    routes
+});
 const app = new Vue({
     el: '#app',
+    mode: history,
+    router
 });
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 @section('content')
     @auth
-    <h1>This is Dashboard</h1>
-    <form action="/logout" method="POST">
-        @csrf
-    <input type="submit" value="log out"></form>
+    <router-view></router-view>
     @endauth
     <h2>error for login</h2>
+    <!--authenticating template-->
+    <not-loggedin></not-loggedin>
 @endsection
