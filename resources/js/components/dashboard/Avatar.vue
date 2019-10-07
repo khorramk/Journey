c971427876471dd9a05f5be9a566aa6be5976bd8
khorramkho <52155201+khorramkho@users.noreply.github.com>
2019-10-07 11:15:06 +0100

    added vuex to maintain resubaility of the avatar component

--- resources/js/components/dashboard/Avatar.vue
+++ resources/js/components/dashboard/Avatar.vue
@@ -1,31 +1,27 @@
 <template>
-    <div>
-        <div class="base-image-input" :style="{ background-image': url(./storage/images/${this.imageData})}" >
-            <span>
+    <div class="avatar-container">
+        <picture class="avatar-container__avatar-picture">
+            <span class="avatar-container__avatar-select-text" v-show="closeUponSelectImage">
                 choose image
             </span>
-            <!--use form to submit a file and upload it-->
-            <form>
-                <input type="file"  name="" id="upload" v-on:change="onSelectFile">
-            </form>
-        </div>
-<!--
-            <input id="imageUpload"   v-on:click="clicked($event)" type="file"
-            name="profile_photo" placeholder="Photo" required="" capture>
-            <input type="image" :src="pathToImage" alt=""> -->
-
-
-
+            <img  class="avatar-container__avatar-img" :src="this.$store.state.avatar.path" alt="avatar" sizes="" width="75" height="75" srcset="" @click="openModal">
+            <div class="modal avatar-container__avatar-modal" v-if="this.$store.state.avatar.open" :style="{width: '400px', height: '300px'}">
+                <input class="avatar-modal__input-avatar base-image-input"  type="file"  name="" id="upload" v-on:change="onSelectFile">
+            </div>
+        </picture>
     </div>
 </template>
-
 <script>
     export default {
         data() {
             return {
                 image: '',
-               imageData: null,
-               file: ''
+               imagePath: '',
+               file: '',
+               imageData: '',
+               file: '',
+               closeUponSelectImage: true,
+               pop: false
             }
         },
         methods: {
@@ -39,19 +35,20 @@
                 this.file = event.target.files[0];
                 console.log(this.file);
                  axios.post('/api/avatars', formData)
-                    .then((resp) => console.log(resp.data))//this.$data.imageData = '/public/storage/avatar1')
-                    .catch((err) => console.log(err));//this.$data.imageData = /asset/image/url);
+                   .then((resp) => {
+                      let path = `./storage/${resp.data}`;
+                    this.$store.dispatch('showImage', path),
+                    this.$data.closeUponSelectImage = false, this.$data.pop = false;})
+                    .catch((err) => console.log(err));
 
             },
-            chooseImage(){
-                console.log(this.$refs);
-                this.$refs.fileInput;
+            openModal(){
+                console.log(this.$store.state.avatar);
+                this.$store.commit('openModal');
             }
         },
+
     }
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
