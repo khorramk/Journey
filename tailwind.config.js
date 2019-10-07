520a9a21b4bad3d8ad643ca8206360b8d868f2e9
khorramkho <52155201+khorramkho@users.noreply.github.com>
2019-10-07 17:54:31 +0100

    configured tailwindcss successfully and styled login component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ module.exports = {
   theme: {
     backgroundColor: theme => ({
         'wood': '#6F6868',
-    })
+        'pale-green': '#92DFAF',
+        'leaf-green': '#D1E9C3',
+        'pure-white': '#FFFFFF',
+    }),
+
+    width: {
+        'bar-size': 'calc(100vw - 20vw)',
+        'login-btn-width': '100px'
+
+    },
+    spacing: {
+        'middle-top-space': '50vh',
+        "left-right-space": 'calc(100vw - 90vw)',
+        'login-inputs-size':'80px',
+        'bottom-spacing': '30px',
+        'login-spacing': '10px',
+        'link-spacing': '10px'
+
+    },
+
   },
   variants: {},
   plugins: []
